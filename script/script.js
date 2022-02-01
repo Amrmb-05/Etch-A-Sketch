057989fd0f8c1cd286a39676901497b33d7c37da
Amr Mohamed <amoree.mb@gmail.com>
2022-02-01 17:20:56 +0200

    made changes to the reset button so that it prompts you ti enter the size of the new grid

--- script/script.js
+++ script/script.js
@@ -1,33 +1,30 @@
-let gridSize = 16
-function createGrid(y) {
+let divs;
+function createGrid(y=12) {
     for (x=1;x<=(y*y);x++) {
         let grid = document.createElement('div');
         document.querySelector('.container').appendChild(grid);
+        
     }
+    divs = document.querySelectorAll('.container>div');
+    squareSize = 832/y - 2 +'px'
+    divs.forEach(div =>{div.style.cssText = `width: ${squareSize}; height: ${squareSize};` })
+    console.log(squareSize)
+    
+    divs.forEach(div => {div.addEventListener('mouseover', () => {
+        div.setAttribute('class', 'hover');
+    })})
+    console.log(divs)
 }
-createGrid(gridSize)
-let divs = document.querySelectorAll('.container>div')
-console.log(divs)
+createGrid()
 
-divs.forEach(div => {div.addEventListener('mouseover', () => {
-    div.setAttribute('class', 'hover');
-})
-});
 
+
+divs = document.querySelectorAll('.container>div')
 let resetBtn = document.querySelector('button');
 resetBtn.addEventListener('click', () => {
-    divs.forEach(div => {div.removeAttribute('class')
-
-    })
-})
-
-let squareSize = 832/gridSize - 2 +'px'
-console.log(squareSize)
-divs.forEach(div =>{div.style.cssText = `width: ${squareSize}; height: ${squareSize}` })
-
-
-resetBtn.addEventListener('click', () => {
-    gridSize = prompt('Enter Gridsize:', )
-    
-})
-
+    divs.forEach(div => div.removeAttribute('class'));
+    divs.forEach(div => div.remove());
+    //console.log(divs)
+    gridSize = prompt('Enter Gridsize:', );
+    createGrid(gridSize);
+});
