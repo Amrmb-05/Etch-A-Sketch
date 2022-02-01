7bc8092ae5d8746c0165fe16478da9fc03f31616
Amr Mohamed <amoree.mb@gmail.com>
2022-02-01 14:15:52 +0200

    added the reset button

--- script/script.js
+++ script/script.js
@@ -1,7 +1,11 @@
-for (x=1;x<=(16*16);x++) {
-    let grid = document.createElement('div');
-    document.querySelector('.container').appendChild(grid);
+let gridSize = 16
+function createGrid(y) {
+    for (x=1;x<=(y*y);x++) {
+        let grid = document.createElement('div');
+        document.querySelector('.container').appendChild(grid);
+    }
 }
+createGrid(gridSize)
 let divs = document.querySelectorAll('.container>div')
 console.log(divs)
 
@@ -9,3 +13,21 @@ divs.forEach(div => {div.addEventListener('mouseover', () => {
     div.setAttribute('class', 'hover');
 })
 });
+
+let resetBtn = document.querySelector('button');
+resetBtn.addEventListener('click', () => {
+    divs.forEach(div => {div.removeAttribute('class')
+
+    })
+})
+
+let squareSize = 832/gridSize - 2 +'px'
+console.log(squareSize)
+divs.forEach(div =>{div.style.cssText = `width: ${squareSize}; height: ${squareSize}` })
+
+
+resetBtn.addEventListener('click', () => {
+    gridSize = prompt('Enter Gridsize:', )
+    
+})
+
